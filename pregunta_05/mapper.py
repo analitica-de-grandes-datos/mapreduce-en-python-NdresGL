bf2a0fa5a22561a5865d9cbac2c6a9e523d7b99d
ANDRES_GOMEZ <angomezlo@unal.edu.co>
2022-07-05 02:30:40 -0500

    Sesion2 Pregunta5

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+for line in sys.stdin:
+    letra,fecha,numero=line.split('   ')
+    dia,mes,ano=fecha.split('-')
+    sys.stdout.write('{}\t1\n'.format(mes)) 
