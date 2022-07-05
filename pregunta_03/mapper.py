6907343685803423e3931305289f0909ddf0cd3b
ANDRES_GOMEZ <angomezlo@unal.edu.co>
2022-07-05 01:58:15 -0500

    Sesion 2 Pregunta 3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        renglon = line.split(',')
+        letra =  renglon[0]
+        numero = renglon[1].replace("\n", "")
+        sys.stdout.write("{}\t{}\n".format(numero,letra))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from ssl import OP_NO_RENEGOTIATION
+import sys
+if __name__ == "__main__":
+    menor = 1000
+    lista=[]
+    Newlist=[]
+    for line in sys.stdin:
+        lista.append(str(line))
+    while len(lista)>0:
+        menor = 1000
+        i = 0
+        for line in lista:
+            numero, letra = str(line).replace('\n','').split('\t')
+            if menor >= int(numero):
+                menor = int(numero)
+                letramin = letra
+                borrar=line
+                i = 1
+        if i == 1:
+            lista.remove(borrar)
+            sys.stdout.write("{},{}\n".format(letramin, menor))
