4dc7454d5f053e4394dd278a183cbb5a9c7cb0f1
ANDRES_GOMEZ <angomezlo@unal.edu.co>
2022-07-04 21:54:39 -0500

    Pregunta 2 Sesion 2
    Pregunta 2 Sesion 2

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        amount = line.split(',')
+        monto =  amount[4]
+        destino = amount[3]
+        sys.stdout.write("{}\t{}\n".format(destino,monto))
