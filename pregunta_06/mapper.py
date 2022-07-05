b6213bb133d81be9da93b62a11722f96d8fed416
ANDRES_GOMEZ <angomezlo@unal.edu.co>
2022-07-05 03:01:14 -0500

    Sesion 2 Pregunta 6

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        renglon = line.split('   ')
+        letra =  renglon[0]
+        fecha =  renglon[1]
+        numero = renglon[2].replace("\n", "")
+        sys.stdout.write("{}\t{}\n".format(letra,numero))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+for line in sys.stdin:
+        key, val = line.split("\t")
+        val = float(val)
+        if key == curkey:
+            if mayor < val:
+                mayor = val
+            if menor > val:
+                menor=val
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey,mayor,menor))
+            curkey = key
+            mayor = float(val)
+            menor=val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, mayor,menor))
