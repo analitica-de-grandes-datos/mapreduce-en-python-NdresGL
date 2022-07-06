90fd26c83f8683c3e4b7552793b3e1b7d3f4883e
ANDRES_GOMEZ <angomezlo@unal.edu.co>
2022-07-05 23:25:52 -0500

    Sesion2 Pregunta 7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        renglon = line.split('   ')
+        letra =  renglon[0]
+        fecha =  renglon[1]
+        numero = int(renglon[2])
+        sys.stdout.write("{}\t{}\t{}\n".format(letra,fecha, numero))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    lista=[]
+    for line in sys.stdin:
+        lista.append(str(line))
+    c=1
+    max = len(lista)^2
+    while len(lista)>0:
+        menor = int(str(lista[0]).replace('\n','').split('\t')[2])
+        i = 0
+        c= c +1
+        key = str(lista[0]).replace('\n','').split('\t')[0]
+        for line in lista:
+            letra,fecha,numero = str(line).replace('\n','').split('\t')
+            if key == letra:
+                if menor>=int(numero):
+                    i =1
+                    menor = int(numero)
+                    fechamin = fecha
+                    borrar = line
+        if c >= max:
+            break
+        if i == 1:
+            lista.remove(borrar)
+            sys.stdout.write('{}   {}   {}\n'.format(key,fechamin,menor))
