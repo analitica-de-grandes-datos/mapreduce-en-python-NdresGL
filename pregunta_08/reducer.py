047054896a81c40d83b651fb97f18530fd025e9d
ANDRES_GOMEZ <angomezlo@unal.edu.co>
2022-07-05 23:43:33 -0500

    Sesion 2 Pregunta 8

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    curkey = None
+    total = 0
+    for line in sys.stdin:
+        letra, fecha, numero = line.split("\t")
+        numero = int(numero)
+        if letra == curkey:
+            total += numero
+            c += 1
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, float(total),total/c))
+            curkey = letra
+            total = numero
+            c= 1
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, float(total),total/c))
